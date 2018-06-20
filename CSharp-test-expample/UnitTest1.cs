4bdc1ecb16f49a9fcc0cc1fbf1db2f41b82feacc
Link789 <33423673+Link789@users.noreply.github.com>
2018-06-20 17:46:07 +0600

    update

--- CSharp-test-expample/UnitTest1.cs
+++ CSharp-test-expample/UnitTest1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 
 namespace CSharp_test_expample
 {
@@ -9,6 +13,20 @@ namespace CSharp_test_expample
         [TestMethod]
         public void TestMethod1()
         {
+            IWebDriver driver = new FirefoxDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(11));
+            driver.Navigate().GoToUrl("http://software-testing.ru");
+            driver.Quit();
+            driver = null;
+           
+        }
+        public void TestMethod2()
+        {
+
+        }
+        public void TestMethod3()
+        {
+
         }
     }
 }
