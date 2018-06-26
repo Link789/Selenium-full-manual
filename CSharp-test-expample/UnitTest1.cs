94206c4c1984572b3f525cd0d71d543c11808e16
Link789 <33423673+Link789@users.noreply.github.com>
2018-06-26 22:58:57 +0600

    Задание 6

--- CSharp-test-expample/UnitTest1.cs
+++ CSharp-test-expample/UnitTest1.cs
@@ -14,12 +14,17 @@ namespace CSharp_test_expample
         public void TestMethod1()
         {
             //задание 3
-            IWebDriver driver = new FirefoxDriver();
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            //IWebDriver driver = new FirefoxDriver();
+            //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            //FirefoxOptions options = new FirefoxOptions();
+            //options.UseLegacyImplementation = true;
+            //driver = new FirefoxDriver(options);
 
             FirefoxOptions options = new FirefoxOptions();
-            options.UseLegacyImplementation = true;
-            driver = new FirefoxDriver(options);
+            options.BrowserExecutableLocation = @"C:\Program Files\Firefox Nightly\firefox.exe";
+            IWebDriver driver = new FirefoxDriver(options);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 
             driver.Navigate().GoToUrl("http://localhost/litecart/admin");
             driver.FindElement(By.Name("username")).SendKeys("admin");
