d3001c3204cadfaf97d65ad52a2be83ed6915e3d
Link789 <33423673+Link789@users.noreply.github.com>
2018-07-01 21:33:27 +0600

    Задание 7

--- CSharp-test-expample/UnitTest1.cs
+++ CSharp-test-expample/UnitTest1.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using System.Collections.ObjectModel;
 
 namespace CSharp_test_expample
 {
     [TestClass]
     public class UnitTest1
-    { 
-    [TestMethod]
+    {
+        public IWebDriver driver;
+        [TestMethod]
         public void TestMethod1()
         {
             //задание 3
@@ -23,7 +26,7 @@ namespace CSharp_test_expample
 
             FirefoxOptions options = new FirefoxOptions();
             options.BrowserExecutableLocation = @"C:\Program Files\Firefox Nightly\firefox.exe";
-            IWebDriver driver = new FirefoxDriver(options);
+            driver = new FirefoxDriver(options);
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 
             driver.Navigate().GoToUrl("http://localhost/litecart/admin");
@@ -34,7 +37,55 @@ namespace CSharp_test_expample
             driver.Quit();
             driver = null;
         }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            driver = new FirefoxDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 
-        
+            driver.Navigate().GoToUrl("http://localhost/litecart/admin");
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+
+            ReadOnlyCollection<IWebElement> linksMenu = driver.FindElements(By.CssSelector("ul#box-apps-menu li#app->a"));
+            string[] listLinks = new string[linksMenu.Count];
+
+            for (int i = 0; i < linksMenu.Count; i++)
+            {
+                listLinks[i] = linksMenu[i].GetAttribute("href");
+            }
+
+            foreach (string link in listLinks)
+            {
+                driver.Navigate().GoToUrl(link);
+                Assert.IsTrue(AreElementPresent(By.CssSelector("td#content h1")));
+
+                ReadOnlyCollection<IWebElement> linksSubMenu = driver.FindElements(By.CssSelector("li.selected li>a"));
+                string[] subListLinks = new string[linksSubMenu.Count];
+
+                for (int i = 0; i < linksSubMenu.Count; i++)
+                {
+                    subListLinks[i] = linksSubMenu[i].GetAttribute("href");
+                }
+
+                foreach (string l in subListLinks)
+                {
+                    driver.Navigate().GoToUrl(l);
+                    Assert.IsTrue(AreElementPresent(By.CssSelector("td#content h1")));
+                }
+            }
+            CloseDriver(driver);
+        }
+
+        public bool AreElementPresent(By locator)
+        {
+            return driver.FindElements(locator).Count > 0;
+        }
+        public void CloseDriver(IWebDriver nameDriver)
+        {
+            nameDriver.Quit();
+        }
+
     }
 }
